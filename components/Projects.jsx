78295cda6947a02ae03f29dffeda6a0dc7f5a40d
emmanuelwangila <emmanuelwangila1@gmail.com>
2024-03-15 16:16:35 +0300

    refactors

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -12,33 +12,40 @@ const Projects = () => {
           Tender Management System
         </h2>
         <div>
-          <p className="dark:text-white flex flex-col ">
+          <p className="dark:text-white flex flex-col m-2  ">
             This is a Tender Management Software system, enabling people to
             create and bid for tenders. Users are able to receive emails upon
             bidding.
-            <a href="https://mern-tender-system.onrender.com/">
-              Visit the system
-            </a>
+            <span className="text-red-400">
+              {" "}
+              <a href="https://mern-tender-system.onrender.com/">
+                Visit the system
+              </a>
+            </span>
           </p>
         </div>
       </div>
 
       <div className="flex flex-col  gap-4 ">
-        <h2 className="dark:text-white ">3D Meta Landing Page</h2>
+        <h2 className="text-blue-500  ">3D Meta Landing Page</h2>
         <div>
-          <p className="dark:text-white flex flex-col ">
+          <p className="dark:text-white flex flex-col m-2  ">
             Virtual Reality Landing Page
-            <a href="http://metacom.vercel.app/">Visit the system</a>
+            <span className="text-red-400">
+              <a href="http://metacom.vercel.app/">Visit the system</a>
+            </span>
           </p>
         </div>
       </div>
 
       <div className="flex flex-col gap-4 ">
-        <h2 className="dark:text-white ">Movie ANimation Clone</h2>
+        <h2 className="text-blue-500 ">Movie ANimation Clone</h2>
         <div>
           <p className="dark:text-white flex flex-col ">
             Animation Clone like hulu, renders movie cards from an api endpoint
-            <a href="http://metacom.vercel.app/">Visit the system</a>
+            <span className="text-red-400">
+              <a href="http://metacom.vercel.app/">Visit the system</a>
+            </span>
           </p>
         </div>
       </div>
