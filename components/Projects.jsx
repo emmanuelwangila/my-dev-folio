4c68b6a236a0eaba3349f0cdbf1ebed8b5723d7b
emmanuelwangila <emmanuelwangila1@gmail.com>
2024-05-29 22:48:59 +0300

    fixing the UI buttons

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -14,17 +14,19 @@ const Projects = () => {
       <div className="flex flex-col  gap-4 ">
         <h2 className=" text-blue-500 ">Tender Management System</h2>
         <div>
-          <p className="dark:text-white flex flex-col m-2  ">
+          <p>
             This is a Tender Management Software system, enabling people to
             create and bid for tenders. Users are able to receive emails upon
             bidding.
-            <span className="text-red-400">
+          </p>
+          <button className="dark:text-white flex bg-blue-500 m-3 p-3  flex-col rounded-md">
+            <span className="text-white">
               {" "}
               <a href="https://mern-tender-system.onrender.com/">
                 Visit the system
               </a>
             </span>
-          </p>
+          </button>
         </div>
       </div>
 
