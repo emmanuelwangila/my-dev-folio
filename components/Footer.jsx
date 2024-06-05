1e34cbe81cc6a80f6c7116ec4ea6eb1b8c4f9643
emmanuelwangila <emmanuelwangila1@gmail.com>
2024-06-05 17:13:27 +0300

    refactor and adding the footer correctly

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,14 +1,11 @@
 const Footer = () => {
   return (
-    <div>
-      <section className=" ">
-        <div className="border  h-full py-20 w-full   lg:flex lg:flex-row  gap-5   lg:w-full md:w-[60%] sm:w-[100%] sm:flex-wrap  border-b border-blue-300 text-blue-600  font-sans  m-4 p-4 rounded-md">
-          <h3>Developedby Emmanuel Wangila</h3>
-          <h4>phone: 0798719417</h4>
-          <h4>@2024 All Rights Reserved</h4>
-        </div>
-      </section>
-      <h2 className="text-blue-200"> Footer </h2>
+    <div className="bg-blue-500 min-h-full text-white rounded-md w-full">
+      <div className="border w-screen h-full py-20 g:flex lg:flex-row lg:w-full md:w-[60%] sm:w-[100%] sm:flex-wrap font-sans rounded-md">
+        <h3>Developed by Emmanuel Wangila</h3>
+        <h4>phone: 0798719417</h4>
+        <h4>@2024 All Rights Reserved</h4>
+      </div>
     </div>
   );
 };
