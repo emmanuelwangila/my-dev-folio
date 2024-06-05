0efbf53494a97c835e3facc060380a04eb860535
emmanuelwangila <emmanuelwangila1@gmail.com>
2024-06-06 01:29:09 +0300

    feat fix finalize designig Footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -12,9 +12,12 @@ const Footer = () => {
     <div className="bg-blue-500 min-h-full flex-col  text-white rounded-md w-full">
       <div className="flex flex-row">
         <div className=" w-screen ml-4  h-full py-20  lg:flex-row lg:w-full md:w-[60%] sm:w-[100%] sm:flex-wrap font-sans rounded-md">
-          <h3 className="m-2 ">Developed by Emmanuel Wangila</h3>
-          <h4 className="m-2 ">phone: 0798719417</h4>
-          <h4 className="m-2">@2024 All Rights Reserved</h4>
+          <h3 className="m-2  text-green-800">
+            {" "}
+            Inspired by: Emmanuel Wangila
+          </h3>
+          <h4 className="m-2  text-green-800"> Reach Me at: 0798719417</h4>
+          <h4 className="m-2 p-2 text-green-800">@2024 All Rights Reserved</h4>
         </div>
         <div className=" ml-4   p-2 lg:flex-col  gap-4 py-10 sm:mx-auto mx-3    ">
           <ul className="flex flex-row m-2 cursor-pointer ">
