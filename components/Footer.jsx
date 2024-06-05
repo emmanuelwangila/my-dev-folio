41f4b3d42e148ebe2a820e595f409616fd6d5a93
emmanuelwangila <emmanuelwangila1@gmail.com>
2024-06-05 20:44:23 +0300

    adding margins to thhe h-s

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -9,10 +9,10 @@ import {
 const Footer = () => {
   return (
     <div className="bg-blue-500 min-h-full text-white rounded-md w-full">
-      <div className=" w-screen h-full py-20 g:flex lg:flex-row lg:w-full md:w-[60%] sm:w-[100%] sm:flex-wrap font-sans rounded-md">
-        <h3>Developed by Emmanuel Wangila</h3>
-        <h4>phone: 0798719417</h4>
-        <h4>@2024 All Rights Reserved</h4>
+      <div className=" w-screen ml-4  h-full py-20 g:flex lg:flex-row lg:w-full md:w-[60%] sm:w-[100%] sm:flex-wrap font-sans rounded-md">
+        <h3 className="m-2 ">Developed by Emmanuel Wangila</h3>
+        <h4 className="m-2 ">phone: 0798719417</h4>
+        <h4 className="m-2">@2024 All Rights Reserved</h4>
       </div>
       <div className=" ml-2  p-2 lg:flex justify-evenly sm:mx-auto mx-3    ">
         <ul className="flex flex-row m-2 cursor-pointer ">
