464cf38674ccff7ded01e0c9920415f78475e62a
emmanuelwangila <emmanuelwangila1@gmail.com>
2024-06-06 01:11:41 +0300

    refactor feat fixes footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -15,16 +15,18 @@ const Footer = () => {
           <h4 className="m-2 ">phone: 0798719417</h4>
           <h4 className="m-2">@2024 All Rights Reserved</h4>
         </div>
-        <div className=" ml-2  p-2 lg:flex-col  justify-evenly sm:mx-auto mx-3    ">
+        <div className=" ml-2  p-2 lg:flex-col  gap-4 py-10 sm:mx-auto mx-3    ">
           <ul className="flex flex-row m-2 cursor-pointer ">
             <a href=" https://x.com/manuelwangila">
               <AiFillTwitterCircle className="text-2xl"></AiFillTwitterCircle>{" "}
-              Twitter
+              <span className="text-white"> Twitter</span>
             </a>
           </ul>
           <ul className="flex flex-row m-2 cursor-pointer">
-            <AiFillLinkedin className="text-2xl cursor-pointer"></AiFillLinkedin>{" "}
-            LInkedIn
+            <a href=" https://www.linkedin.com/in/emmanuel-wangila/">
+              <AiFillLinkedin className="text-2xl cursor-pointer"></AiFillLinkedin>{" "}
+              LInkedIn
+            </a>
           </ul>
           <ul className="cursor-pointer">
             {" "}
