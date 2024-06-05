3e997735fddce2a323c3c458f3ddea2929a3cf1a
emmanuelwangila <emmanuelwangila1@gmail.com>
2024-06-06 01:06:50 +0300

    feat fix refactoring the foooter

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -8,31 +8,33 @@ import {
 
 const Footer = () => {
   return (
-    <div className="bg-blue-500 min-h-full text-white rounded-md w-full">
-      <div className=" w-screen ml-4  h-full py-20 g:flex lg:flex-row lg:w-full md:w-[60%] sm:w-[100%] sm:flex-wrap font-sans rounded-md">
-        <h3 className="m-2 ">Developed by Emmanuel Wangila</h3>
-        <h4 className="m-2 ">phone: 0798719417</h4>
-        <h4 className="m-2">@2024 All Rights Reserved</h4>
-      </div>
-      <div className=" ml-2  p-2 lg:flex justify-evenly sm:mx-auto mx-3    ">
-        <ul className="flex flex-row m-2 cursor-pointer ">
-          <a href=" https://x.com/manuelwangila">
-            <AiFillTwitterCircle className="text-2xl"></AiFillTwitterCircle>{" "}
-            Twitter
-          </a>
-        </ul>
-        <ul className="flex flex-row m-2 cursor-pointer">
-          <AiFillLinkedin className="text-2xl cursor-pointer"></AiFillLinkedin>{" "}
-          LInkedIn
-        </ul>
-        <ul className="cursor-pointer">
-          {" "}
-          {/* <AiFillGmail className="text-2xl "></AiFillGmail> */}
-          emmanuelwangila1@gmail.com
-        </ul>
-        <ul className="flex flex-row m-2  cursor-pointer">
-          <AiFillGithub className="text-2xl"></AiFillGithub> Github
-        </ul>
+    <div className="bg-blue-500 min-h-full flex-col  text-white rounded-md w-full">
+      <div className="flex flex-row">
+        <div className=" w-screen ml-4  h-full py-20  lg:flex-row lg:w-full md:w-[60%] sm:w-[100%] sm:flex-wrap font-sans rounded-md">
+          <h3 className="m-2 ">Developed by Emmanuel Wangila</h3>
+          <h4 className="m-2 ">phone: 0798719417</h4>
+          <h4 className="m-2">@2024 All Rights Reserved</h4>
+        </div>
+        <div className=" ml-2  p-2 lg:flex-col  justify-evenly sm:mx-auto mx-3    ">
+          <ul className="flex flex-row m-2 cursor-pointer ">
+            <a href=" https://x.com/manuelwangila">
+              <AiFillTwitterCircle className="text-2xl"></AiFillTwitterCircle>{" "}
+              Twitter
+            </a>
+          </ul>
+          <ul className="flex flex-row m-2 cursor-pointer">
+            <AiFillLinkedin className="text-2xl cursor-pointer"></AiFillLinkedin>{" "}
+            LInkedIn
+          </ul>
+          <ul className="cursor-pointer">
+            {" "}
+            {/* <AiFillGmail className="text-2xl "></AiFillGmail> */}
+            emmanuelwangila1@gmail.com
+          </ul>
+          <ul className="flex flex-row m-2  cursor-pointer">
+            <AiFillGithub className="text-2xl"></AiFillGithub> Github
+          </ul>
+        </div>
       </div>
     </div>
   );
