2d6a5eda949093583dcc32d383a6e3b74be7094e
emmanuelwangila <emmanuelwangila1@gmail.com>
2024-06-06 01:26:06 +0300

    code clean-ups footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -16,27 +16,29 @@ const Footer = () => {
           <h4 className="m-2 ">phone: 0798719417</h4>
           <h4 className="m-2">@2024 All Rights Reserved</h4>
         </div>
-        <div className=" ml-2  p-2 lg:flex-col  gap-4 py-10 sm:mx-auto mx-3    ">
+        <div className=" ml-4   p-2 lg:flex-col  gap-4 py-10 sm:mx-auto mx-3    ">
           <ul className="flex flex-row m-2 cursor-pointer ">
             <a href=" https://x.com/manuelwangila">
               <AiFillTwitterCircle className="text-2xl"></AiFillTwitterCircle>{" "}
               <span className="text-white"> Twitter</span>
             </a>
           </ul>
-          <ul className="flex flex-row m-2 cursor-pointer">
+          <ul className="flex  m-2  cursor-pointer">
             <a href=" https://www.linkedin.com/in/emmanuel-wangila/">
               <AiFillLinkedin className="text-2xl cursor-pointer"></AiFillLinkedin>{" "}
               LInkedIn
             </a>
           </ul>
-          <ul className="cursor-pointer">
+          {/* <ul className="cursor-pointer flex ">
             <a href="https://mail.google.com/">
               <BiLogoGmail></BiLogoGmail>
-              emmanuelwangila1@gmail.com
+              <span className="text-xs "> emmanuelwangila1@gmail.com</span>
             </a>
-          </ul>
+          </ul> */}
           <ul className="flex flex-row m-2  cursor-pointer">
-            <AiFillGithub className="text-2xl"></AiFillGithub> Github
+            <a href="https:github.com/emmanuelwangila">
+              <AiFillGithub className="text-2xl"></AiFillGithub> Github
+            </a>
           </ul>
         </div>
       </div>
