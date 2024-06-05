fada3a3c752ecbe1a092e63ac921e1cdee5d1a92
emmanuelwangila <emmanuelwangila1@gmail.com>
2024-06-05 17:50:25 +0300

    refactor responsiveness Twitter.. footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,10 +1,16 @@
 const Footer = () => {
   return (
     <div className="bg-blue-500 min-h-full text-white rounded-md w-full">
-      <div className="border w-screen h-full py-20 g:flex lg:flex-row lg:w-full md:w-[60%] sm:w-[100%] sm:flex-wrap font-sans rounded-md">
+      <div className=" w-screen h-full py-20 g:flex lg:flex-row lg:w-full md:w-[60%] sm:w-[100%] sm:flex-wrap font-sans rounded-md">
         <h3>Developed by Emmanuel Wangila</h3>
         <h4>phone: 0798719417</h4>
         <h4>@2024 All Rights Reserved</h4>
+      </div>
+      <div className=" ml-2  p-2 lg:flex justify-evenly sm:mx-auto mx-3    ">
+        <ul>Twitter</ul>
+        <ul>LInkedIn</ul>
+        <ul>emmanuelwangila1@gmail.com</ul>
+        <ul>Github</ul>
       </div>
     </div>
   );
