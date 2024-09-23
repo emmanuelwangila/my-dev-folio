ddde9690ea770f14c0bd9961a194cc0780db0567
emmanuelwangila <emmanuelwangila1@gmail.com>
2024-09-23 21:05:26 +0300

    refactors

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -19,28 +19,36 @@ const Footer = () => {
           <h4 className="m-2  text-green-800"> Reach Me at: 0798719417</h4>
           <h4 className="m-2 p-2 text-green-800">@2024 All Rights Reserved</h4>
         </div>
-        <div className=" ml-4   p-2 lg:flex-col  gap-4 py-10 sm:mx-auto mx-3    ">
-          <ul className="flex flex-row m-2 cursor-pointer ">
-            <a href=" https://x.com/manuelwangila">
-              <AiFillTwitterCircle className="text-2xl"></AiFillTwitterCircle>{" "}
-              <span className="text-white"> Twitter</span>
+        <div className="flex justify-center sm:mx-auto items-center lg:mr-20  py-10">
+          <ul className="flex space-x-8 cursor-pointer">
+            <a
+              href="https://x.com/manuelwangila"
+              className="flex items-center space-x-2 transform hover:scale-105 transition-transform"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <AiFillTwitterCircle className="text-4xl" />
+              <span className="text-white">Twitter</span>
             </a>
-          </ul>
-          <ul className="flex  m-2  cursor-pointer">
-            <a href=" https://www.linkedin.com/in/emmanuel-wangila/">
-              <AiFillLinkedin className="text-2xl cursor-pointer"></AiFillLinkedin>{" "}
-              LInkedIn
+
+            <a
+              href="https://www.linkedin.com/in/emmanuel-wangila/"
+              className="flex items-center space-x-2 transform hover:scale-105 transition-transform"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <AiFillLinkedin className="text-4xl" />
+              <span className="text-white">LinkedIn</span>
             </a>
-          </ul>
-          {/* <ul className="cursor-pointer flex ">
-            <a href="https://mail.google.com/">
-              <BiLogoGmail></BiLogoGmail>
-              <span className="text-xs "> emmanuelwangila1@gmail.com</span>
-            </a>
-          </ul> */}
-          <ul className="flex flex-row m-2  cursor-pointer">
-            <a href="https://github.com/emmanuelwangila">
-              <AiFillGithub className="text-2xl"></AiFillGithub> Github
+
+            <a
+              href="https://github.com/emmanuelwangila"
+              className="flex items-center space-x-2 transform hover:scale-105 transition-transform"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <AiFillGithub className="text-4xl" />
+              <span className="text-white">Github</span>
             </a>
           </ul>
         </div>
